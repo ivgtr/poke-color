b1825c65a163b60ac334fc1d303585db14dbd0ae
ivgtr <gear.1112t.o@gmail.com>
2020-08-20 19:25:09 +0900

    :package: add: packageを追加し忘れていた

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true
+  },
   theme: {},
   variants: {},
   plugins: [],
