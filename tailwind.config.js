01da43447af758efb2462a93c8fbf3c577c93396
ivgtr <gear.1112t.o@gmail.com>
2020-08-28 16:22:02 +0900

    :ambulance: fix: tailwindの設定ミスを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,13 @@ module.exports = {
   variants: {},
   plugins: [],
   purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
     content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
+      './src/components/**/*.vue',
+      './src/layouts/**/*.vue',
+      './src/pages/**/*.vue',
+      './src/plugins/**/*.js',
+      './src/nuxt.config.js'
     ]
   }
 }
