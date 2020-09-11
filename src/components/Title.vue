bec36d35319ef23373c7a2e204ba06e6bff19f43
ivgtr <gear.1112t.o@gmail.com>
2020-09-11 18:47:41 +0900

    :lipstick: change: titleのstyle調整

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="title text-center">
-    <h1 class="text-5xl font-bold mt-12">Poke Color</h1>
+    <h1 class="text-5xl font-bold mt-12">PokeColor</h1>
     <p class="text-xs italic opacity-75">
       11/09/2020 - カントー地方のポケモンまで追加済
     </p>
@@ -15,6 +15,8 @@
       游ゴシック体, 游ゴシック, メイリオ, meiryo, ｍｓ ｐゴシック, ms pgothic,
       sans-serif;
     color: #f7cc47;
+    -webkit-text-stroke: 1px #3e60a7;
+    letter-spacing: 1px;
   }
 }
 </style>
