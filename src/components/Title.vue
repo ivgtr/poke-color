4cb9f836f30dfd9139c4f4647f41deb2d09c4582
ivgtr <gear.1112t.o@gmail.com>
2020-09-11 18:40:54 +0900

    :lipstick: change: fontの修正

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="title text-center">
     <h1 class="text-5xl font-bold mt-12">Poke Color</h1>
-    <p class="italic opacity-75">
+    <p class="text-xs italic opacity-75">
       11/09/2020 - カントー地方のポケモンまで追加済
     </p>
   </div>
@@ -10,7 +10,11 @@
 <style lang="scss" scoped>
 .title {
   & > h1 {
-    font-family: 'Passion One', cursive;
+    font-family: 'Passion One', -apple-system, blinkmacsystemfont,
+      ヒラギノ角ゴ pro w3, hiragino kaku gothic pro, roboto, yugothic, yu gothic,
+      游ゴシック体, 游ゴシック, メイリオ, meiryo, ｍｓ ｐゴシック, ms pgothic,
+      sans-serif;
+    color: #f7cc47;
   }
 }
 </style>
