3f7d7689da4c725ff9295dfae0de84fbd58e6a58
ivgtr <gear.1112t.o@gmail.com>
2020-09-14 15:02:18 +0900

    :mag: change: metaタグの変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,15 @@ const config = {
   mode: 'universal',
   target: 'static',
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'PokeColor',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content:
+          'あのポケモンの色を使ってみたいと思ったことはありませんか？このサイトでは、あなたの好きなポケモンのカラーコードを自由に入手することができます。'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
